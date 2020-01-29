5877e16c6898bc876ed79f7f8c85561407ef42a4
CHunYenc <yuhnoesty0402@hotmail.com.tw>
2020-01-29 05:27:28 -0800

    main code

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 # Create your views here.
 
@@ -8,3 +10,19 @@ def index(request):
 
 def test1(request):
     return HttpResponse('Hello Yen!')
+
+def post_list(request):
+    posts = Post.published.all()
+    paginator = Paginator(posts,3) # 顯示 3 篇文章
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger: #若 page 不是一個整數則返回第一頁
+        posts = paginator.page(1)
+    except EmptyPage: #若 page 超出
+        posts = paginator.page(paginator.num_pages)
+    return render(request,'blog/post/list.html',{'page':page,'posts':posts})
+
+def post_detail(request,year,month,day,post):
+    post = get_object_or_404(Post,slug=post,status="published",publish__year=year,publish__month=month,publish__day=day)
+    return render(request,'blog/post/detail.html',{'post':post})
