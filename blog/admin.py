c3293251b26ded4f1d392a5220876968a90af7ae
CHunYenc <yuhnoesty0402@hotmail.com.tw>
2020-01-29 05:20:47 -0800

    Add ModelManager

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title','slug','publish','status')
+    list_display = ('title','slug','publish','status',)
     '''
     list_filter = ('status', 'created', 'publish', 'author',)
     search_fields = ('title', 'body',)
