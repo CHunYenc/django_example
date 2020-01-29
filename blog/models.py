2efdf2abc62476ae1d0483626bd4a547a759a842
CHunYenc <yuhnoesty0402@hotmail.com.tw>
2020-01-29 05:27:59 -0800

    databases

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (('draft','Draft'),('published','Published'))
@@ -12,10 +17,15 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='published')
+    objects = models.Manager()  # 默认的管理器
+    published = PublishedManager()  # 自定义管理器
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
